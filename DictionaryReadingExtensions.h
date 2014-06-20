88f57cef23a83120730991b5dce529b08bc5eb73
Jonathan Taylor <jonathan.taylor@glasgow.ac.uk>
2014-06-20 12:13:30 +0100

    add NSDictionary category for reading numbers

---
+++ DictionaryReadingExtensions.h
@@ -0,0 +1,24 @@
+//
+//  DictionaryReadingExtensions.h
+//  Spim Interface
+//
+//  Created by Jonathan Taylor on 20/06/2014.
+//
+//
+
+#ifndef Spim_Interface_DictionaryReadingExtensions_h
+#define Spim_Interface_DictionaryReadingExtensions_h
+
+@interface NSDictionary (CompulsoryKeyReading)
+
+-(NSNumber *)getRequiredNumberForKey:(NSString *)key;
+-(NSNumber *)getOptionalNumberForKey:(NSString *)key defaultVal:(NSNumber *)def;
+-(int)getRequiredIntForKey:(NSString *)key;
+-(double)getRequiredDoubleForKey:(NSString *)key;
+-(double)getOptionalDoubleForKey:(NSString *)key defaultVal:(double)def;
+-(int)getOptionalIntForKey:(NSString *)key defaultVal:(int)def;
+-(bool)getOptionalBoolForKey:(NSString *)key defaultVal:(bool)def;
+
+@end
+
+#endif
---
+++ DictionaryReadingExtensions.mm
@@ -0,0 +1,65 @@
+//
+//  DictionaryReadingExtensions.m
+//  Spim Interface
+//
+//  Created by Jonathan Taylor on 20/06/2014.
+//
+//
+
+#import "DictionaryReadingExtensions.h"
+
+@implementation NSDictionary (CompulsoryKeyReading)
+
+-(NSNumber *)getRequiredNumberForKey:(NSString *)key
+{
+	ALWAYS_ASSERT([self isKindOfClass:[NSDictionary class]]);
+	NSNumber *num = [self objectForKey:key];
+	ALWAYS_ASSERT([num isKindOfClass:[NSNumber class]]);
+	return num;
+}
+
+-(NSNumber *)getOptionalNumberForKey:(NSString *)key defaultVal:(NSNumber *)def
+{
+	ALWAYS_ASSERT([self isKindOfClass:[NSDictionary class]]);
+	NSNumber *num = [self objectForKey:key];
+    if (num == nil)
+        return def;
+	ALWAYS_ASSERT([num isKindOfClass:[NSNumber class]]);
+	return num;
+}
+
+-(int)getRequiredIntForKey:(NSString *)key
+{
+    return [self getRequiredNumberForKey:key].intValue;
+}
+
+-(double)getRequiredDoubleForKey:(NSString *)key
+{
+    return [self getRequiredNumberForKey:key].doubleValue;
+}
+
+-(double)getOptionalDoubleForKey:(NSString *)key defaultVal:(double)def
+{
+    NSNumber *num = [self getOptionalNumberForKey:key defaultVal:nil];
+    if (num == nil)
+        return def;
+	return num.doubleValue;
+}
+
+-(int)getOptionalIntForKey:(NSString *)key defaultVal:(int)def
+{
+    NSNumber *num = [self getOptionalNumberForKey:key defaultVal:nil];
+    if (num == nil)
+        return def;
+	return num.intValue;
+}
+
+-(bool)getOptionalBoolForKey:(NSString *)key defaultVal:(bool)def
+{
+    NSNumber *num = [self getOptionalNumberForKey:key defaultVal:nil];
+    if (num == nil)
+        return def;
+	return num.boolValue;
+}
+
+@end
