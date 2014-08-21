6e142a12be1d06d1dfa625c1d34bc38bc81a3a9a
Jonathan Taylor <jonathan.taylor@glasgow.ac.uk>
2014-08-21 14:30:14 +0100

    got into a mess due to not staying up to date with remote - clean up!

--- DictionaryReadingExtensions.h
+++ DictionaryReadingExtensions.h
@@ -20,7 +20,6 @@
 -(double)getOptionalDoubleForKey:(NSString *)key defaultVal:(double)def;
 -(int)getOptionalIntForKey:(NSString *)key defaultVal:(int)def;
 -(bool)getOptionalBoolForKey:(NSString *)key defaultVal:(bool)def;
--(NSString*)getRequiredStringForKey:(NSString *)key;
 
 @end
 
--- DictionaryReadingExtensions.mm
+++ DictionaryReadingExtensions.mm
@@ -75,12 +75,4 @@
 	return num.boolValue;
 }
 
--(NSString*)getRequiredStringForKey:(NSString *)key
-{
-	ALWAYS_ASSERT([self isKindOfClass:[NSDictionary class]]);
-	NSString *str = [self objectForKey:key];
-	ALWAYS_ASSERT([str isKindOfClass:[NSString class]]);
-	return str;
-}
-
 @end
