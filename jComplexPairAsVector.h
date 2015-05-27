fc29743f6a8c851fb617f38f06d481802fd0b44f
Jonathan Taylor <jonathan.taylor@glasgow.ac.uk>
2015-05-27 11:57:54 +0100

    Upgrade jComplexPairAsVector to match jComplexPairAsVector256 enhancements

--- jComplexPairAsVector.h
+++ jComplexPairAsVector.h
@@ -29,10 +29,15 @@ class jComplexPairAsVector
 	jComplex b(void) const { return jComplex(vUpper(__re), vUpper(__im)); }
 	vDouble re(void) const { return __re; }
 	vDouble im(void) const { return __im; }
+    double a_r(void) const { return __re[0]; }
+    double a_i(void) const { return __im[0]; }
+    double b_r(void) const { return __re[1]; }
+    double b_i(void) const { return __im[1]; }
 
 	explicit jComplexPairAsVector(const jComplex &inZ) { __re = (vDouble){real(inZ), real(inZ)}; __im = (vDouble){imag(inZ), imag(inZ)}; }
 	jComplexPairAsVector(const jComplexPairAsVector &inAB) { __re = inAB.re(); __im = inAB.im(); }
 	jComplexPairAsVector(jComplex inA, jComplex inB) { __re = (vDouble){real(inA), real(inB)}; __im = (vDouble){imag(inA), imag(inB)}; }
+    jComplexPairAsVector(double ar, double ai, double br, double bi) { __re = (vDouble){ar, br}; __im = (vDouble){ai, bi}; }
 	jComplexPairAsVector(vDouble inRe, vDouble inIm) { __re = inRe; __im = inIm; }
 	
 	void SetA(jComplex inA) { __re = (vDouble){real(inA), real(b())}; __im = (vDouble){imag(inA), imag(b())}; }
@@ -105,6 +110,8 @@ class jComplexPairAsVector
 		return vHAddLower(sum2);
 	}
 	jComplex SumAcross(void) const { return jComplex(SumReal(), SumImag()); }		// Return the sum of the two values as a single complex number
+    jComplexPairAsVector Negated(double ar, double ai, double br, double bi) const { return jComplexPairAsVector(_mm_mul_pd(__re, (vDouble){ar, br}), _mm_mul_pd(__im, (vDouble){ai, bi})); }
+    jComplexPairAsVector NegatedUsingXORWith(jComplexPairAsVector neg) const { return jComplexPairAsVector(_mm_xor_pd(__re, neg.re()), _mm_xor_pd(__im, neg.im())); }   // Expects a vector containing either 0.0 or -0.0 entries. Uses the XOR operator to negate self according to that pattern
 	jComplexPairAsVector GetNegative(void) const { return jComplexPairAsVector(vNegate(__re), vNegate(__im)); }		// Return the negated values of a and b
 	jComplexPairAsVector GetNegativeOfSecondOnly(void) const { return jComplexPairAsVector(_mm_xor_pd(__re, (vDouble) { 0.0, -0.0 }), _mm_xor_pd(__im, (vDouble) { 0.0, -0.0 })); }		// Return the negated value of b, leaving a unchanged
     jComplexPairAsVector GetNegativeOfFirstOnly(void) const { return jComplexPairAsVector(_mm_xor_pd(__re, (vDouble) { -0.0, 0.0 }), _mm_xor_pd(__im, (vDouble) { -0.0, 0.0 })); }		// Return the negated value of a, leaving b unchanged
