160618f84476f37eaddaeb14c1873cd90e510607
Jonny Taylor <jonathan.taylor@glasgow.ac.uk>
2016-06-06 16:01:08 +0100

    Add uint16 support for jPythonArray

--- jPythonArray.cpp
+++ jPythonArray.cpp
@@ -4,3 +4,4 @@ template<> int ArrayType<double>(void) { return NPY_DOUBLE; }
 template<> int ArrayType<float>(void) { return NPY_FLOAT; }
 template<> int ArrayType<int>(void) { return NPY_INT32; }
 template<> int ArrayType<unsigned char>(void) { return NPY_UBYTE; }
+template<> int ArrayType<unsigned short>(void) { return NPY_USHORT; }
