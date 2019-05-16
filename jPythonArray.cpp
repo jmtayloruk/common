e3a1f311f794c6de1231307c91377bf286132b74
Jonny Taylor <jonathan.taylor@glasgow.ac.uk>
2019-05-16 09:59:26 +0100

    Add python array support for complex types

--- jPythonArray.cpp
+++ jPythonArray.cpp
@@ -1,8 +1,11 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include "jPythonArray.h"
+#include <complex>
 
 template<> int ArrayType<double>(void) { return NPY_DOUBLE; }
 template<> int ArrayType<float>(void) { return NPY_FLOAT; }
 template<> int ArrayType<int>(void) { return NPY_INT32; }
 template<> int ArrayType<unsigned char>(void) { return NPY_UBYTE; }
 template<> int ArrayType<unsigned short>(void) { return NPY_USHORT; }
+template<> int ArrayType< std::complex<float> >(void) { return NPY_CFLOAT; }
+template<> int ArrayType< std::complex<double> >(void) { return NPY_CDOUBLE; }
