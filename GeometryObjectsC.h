7f4994caf0c5087bfaf881d4c6834fb9566b1d3a
Jonny Taylor <jonathan.taylor@glasgow.ac.uk>
2020-03-21 23:16:08 +0000

    Introduce IntegerPoint3D class

--- GeometryObjectsC.h
+++ GeometryObjectsC.h
@@ -25,4 +25,20 @@ inline bool operator!=(IntegerPoint a, IntegerPoint b) { return ((a.x != b.x) ||
 inline IntegerPoint operator+(IntegerPoint a, IntegerPoint b) { return IntegerPoint(a.x+b.x, a.y+b.y); }
 inline IntegerPoint operator-(IntegerPoint a, IntegerPoint b) { return IntegerPoint(a.x-b.x, a.y-b.y); }
 
+struct IntegerPoint3D
+{
+    int x, y, z;
+    IntegerPoint3D() : x(0), y(0), z(0) { }
+    IntegerPoint3D(int a, int b, int c) : x(a), y(b), z(c) { }
+    IntegerPoint3D& operator += (IntegerPoint3D n) { x += n.x; y += n.y; z += n.z; return *this; }
+    IntegerPoint3D& operator -= (IntegerPoint3D n) { x -= n.x; y -= n.y; z -= n.z; return *this; }
+};
+
+// Sadly I can't seem to get pass-by-reference to work with this - some of my ObjC property-based
+// code doesn't compile if I pass a and b by reference. Not sure if this would be fix-able, but
+// I'm just going to leave it as-is for now.
+inline bool operator!=(IntegerPoint3D a, IntegerPoint3D b) { return ((a.x != b.x) || (a.y != b.y) || (a.z != b.z)); }
+inline IntegerPoint3D operator+(IntegerPoint3D a, IntegerPoint3D b) { return IntegerPoint3D(a.x+b.x, a.y+b.y, a.z+b.z); }
+inline IntegerPoint3D operator-(IntegerPoint3D a, IntegerPoint3D b) { return IntegerPoint3D(a.x-b.x, a.y-b.y, a.z-b.z); }
+
 #endif
