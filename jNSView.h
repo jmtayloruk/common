2298094e8d5daae3d3869dbc4efbb8cdba38e5d7
Jonny <jonny@Jonnys-MacBook-Air-7.local>
2026-08-12 10:29:54 +0100

    Add comment explaining purpose of JNSView class

--- jNSView.h
+++ jNSView.h
@@ -4,6 +4,9 @@
 //
 //  Created by Jonathan Taylor on 22/05/2015.
 //
+//  A light wrapper around NSView that implements viewNeedsRedraw_dummyProperty as a convenient way
+//  to handle redrawing when a dependent property changes.
+//  Subclasses should implement keyPathsForValuesAffectingValueForKey to define which properties should trigger a redraw.
 //
 
 #import <Cocoa/Cocoa.h>
